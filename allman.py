0d77f50cc79163c112a6d5bac376f6144b31e874
Ryan Matlock <ryan.matlock@gmail.com>
2014-07-15 12:55:28 -0700

    basic Allman style braces are working

--- allman.py
+++ allman.py
@@ -11,6 +11,8 @@ the new line with the opening whitespace + the opening curly brace.
 """
 
 import re
+import sys
+import os
 
 # p = re.compile(r"^(?P<ws>foo)(?P<body>bar)$")
 # test = "foobar"
@@ -26,3 +28,37 @@ test = "    h2 {"
 #     print("next line: '" +  m.group("ws") + "{'")
 # else:
 #     print("no match")
+
+if len(sys.argv) > 1:
+    fname = sys.argv[1]
+else:
+    while True:
+        fname = input("Enter filename to process: ")
+        if fname:
+            break
+
+fname = os.path.expanduser(fname)
+
+lines = []
+
+with open(fname) as f:
+    # don't call readlines -- just iterate over file instead
+    # see: http://stupidpythonideas.blogspot.com/2013/06/readlines-considered-silly.html
+    for line in f:
+        m = p.match(line)
+        if m is not None:
+            lines.append(m.group("ws") + m.group("body") + "\n")
+            lines.append(m.group("ws") + "{\n")
+        else:
+            lines.append(line)
+
+# grab the filename without the extension + the extension
+out_dir = os.path.dirname(fname)
+out_fname, ext = os.path.splitext(os.path.basename(fname))
+out_fname += "-allman" + ext
+
+with open(os.path.join(out_dir, out_fname), "w") as out:
+    for line in lines:
+        out.write(line)
+
+print(out_fname + " successfully written to ." + out_dir)
